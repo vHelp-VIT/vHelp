aa4cf421045f0bb3fcd4e6409052b4f6156a4ea7
Shivansh <shivanshgoel21@gmail.com>
2020-07-29 18:59:26 +0100

    update

--- public/typing.js
+++ public/typing.js
@@ -2,18 +2,18 @@
 // typed.js is a required module for this file
 // <!-- credit typed.js: https://github.com/mattboldt/typed.js/ -->
 
-let namaste='Hello';
-let namaste_french='Hello';
-let namaste_punjabi='ਸਤ ਸ੍ਰੀ ਅਕਾਲ';
-let namaste_tamil='வணக்கம்';
-let namaste_3='హలో';
+let namaste='Hello Bounjour';
+// let namaste_french='';
+// let namaste_punjabi='ਸਤ ਸ੍ਰੀ ਅਕਾਲ';
+// let namaste_tamil='வணக்கம்';
+// let namaste_3='హలో';
 let love_line2="vHelp is an open discussion forum for VITians";
 //let love_line='The Forum Made with <i class="fa fa-heart-o" style="font-size:48px;color:red"></i> by VITians';
 let love_line='Made with ❤️ by VITians for VITians';
 let ask_vitian="Clear your Queries directly from VITians";
 let redir="Redirect to Already Answered Queries!!"
 var typed=new Typed('#typewriter_text',{
-    strings:[namaste,namaste_tamil,namaste_punjabi,namaste_3,namaste_french,love_line2,love_line,ask_vitian,redir],
+    strings:[namaste,love_line2,love_line,ask_vitian,redir],
     typeSpeed:30,
     backSpeed:70,
     loop:true
