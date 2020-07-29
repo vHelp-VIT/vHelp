47f925d5e122d91c90fda79a99d9079ef86b612c
Shivansh <shivanshgoel21@gmail.com>
2020-07-29 15:59:29 +0530

    tagline

--- public/typing.js
+++ public/typing.js
@@ -2,13 +2,13 @@
 // typed.js is a required module for this file
 // <!-- credit typed.js: https://github.com/mattboldt/typed.js/ -->
 
-let namaste='Hello bonjour नमस्ते';
+let namaste='Hello bonjour नमस्ते হ্যালো ਸਤ ਸ੍ਰੀ ਅਕਾਲ வணக்கம் హలో';
 let love_line2="vHelp is an open discussion forum for VIT'ians";
 let love_line='The Forum Made with <i class="fa fa-heart-o" style="font-size:48px;color:red"></i> by Vitians';
 let ask_vitian="Clear your Queries directly from VIT'ians";
 let redir="Redirect to Already Answered Queries!!"
 var typed=new Typed('#typewriter_text',{
-    strings:[namaste,love_line,love_line2,ask_vitian,redir],
+    strings:[namaste,love_line2,love_line,ask_vitian,redir],
     typeSpeed:30,
     backSpeed:70,
     loop:true
