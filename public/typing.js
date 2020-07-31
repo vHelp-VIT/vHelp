f92af209a0407847a6c5dad3907b1e7d1292cf30
Shivansh <shivanshgoel21@gmail.com>
2020-07-31 11:00:17 +0100

    card fixed

--- public/typing.js
+++ public/typing.js
@@ -8,12 +8,13 @@ let namaste='Hello, Bounjour, नमस्ते 🙏';
 // let namaste_tamil='வணக்கம்';
 // let namaste_3='హలో';
 let love_line2="vHelp is an open discussion forum for VITians";
+let welcome="Weclome to vHelp"
 //let love_line='The Forum Made with <i class="fa fa-heart-o" style="font-size:48px;color:red"></i> by VITians';
 let love_line='Made with ❤️ by VITians for VITians';
 let ask_vitian="Clear your Queries directly from VITians";
 let redir="Redirect to Already Answered Queries!!"
 var typed=new Typed('#typewriter_text',{
-    strings:[namaste,love_line2,love_line,ask_vitian,redir],
+    strings:[namaste,welcome,love_line2,love_line,ask_vitian,redir],
     typeSpeed:30,
     backSpeed:70,
     loop:true
